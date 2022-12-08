d754aac050b2f6ad233edce2acd33ed8f6944883
MohssineElAttari <m.elattari@digency.ma>
2022-12-08 22:39:16 +0100

    first commit

---
+++ src/main/java/com/indatacore/reast_api_indatacore/ReastApiIndatacoreApplication.java
@@ -0,0 +1,13 @@
+package com.indatacore.reast_api_indatacore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReastApiIndatacoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReastApiIndatacoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/indatacore/reast_api_indatacore/ReastApiIndatacoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indatacore.reast_api_indatacore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReastApiIndatacoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
